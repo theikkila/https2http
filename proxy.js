536a9cda332bdba444cfc40a757552b8d44d82d6
Denis <43764701+zlocate@users.noreply.github.com>
2019-11-22 00:24:59 +0300

    Fix https links

--- proxy.js
+++ proxy.js
@@ -5,8 +5,11 @@ var request = require('request');
 var server = http.createServer(function (req, resp) {
     var x = request({strictSSL: false, url:config.remote + req.url})
     req.pipe(x)
+    if(resp && resp.body) fixlinks(resp.body);
     x.pipe(resp);
 });
+
+function fixlinks(body) => { body.replace("https://","http://")}
 
 
 server.listen(config.port, function(){
